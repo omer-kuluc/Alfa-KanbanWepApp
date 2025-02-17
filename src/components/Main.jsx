2c278b7cebef9896a9f0a74055a35a6f7a1ce345
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-02-17 11:32:36 +0300

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,13 +8,20 @@ import Sidebar from "./Sidebar";
 
 export default function Main() {
   const { data, setData } = useContext(Data);
-  const [selectedBoard, setSelectedBoard] = useState(data[0].name);
+  const [selectedBoard, setSelectedBoard] = useState(data[0]);
 
   return (
     <>
-      <Header selectedBoard={selectedBoard} />
+      {/* <Header selectedBoard={selectedBoard} /> */}
       <main>
         <Sidebar selectedBoard={selectedBoard} setSelectedBoard={setSelectedBoard} />
+        <div className="main-grid-column-group">
+          {selectedBoard.columns.map((column) => 
+            <div key={column.id} className="main-grid-column-item">
+              <h2 className="main-grid-column-title">{column.name} ({column.tasks.length})</h2>
+            </div>
+          )}
+        </div>
       </main>
     </>
   );
